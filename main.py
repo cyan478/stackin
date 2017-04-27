1da3c45720bf770ffacc947c47fc679691a0c018
cyan478 <cyan2@stuy.edu>
2017-04-27 09:18:56 -0400

    D

---
+++ main.py
@@ -0,0 +1,18 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+polygons = []
+transform = new_matrix()
+
+# print_matrix( make_bezier() )
+# print
+# print_matrix( make_hermite() )
+# print
+
+parse_file( 'script', edges, polygons, transform, screen, color )
